ad562eb6425927971df59aa9517fb50fda1f8d65
NellyCN <njanetcumpa@gmail.com>
2023-03-06 15:20:09 -0500

    user, comment y css

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,11 +1,13 @@
 // REGISTRAR NUEVOS USUARIOS
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
-import { getDocs, collection, addDoc, query, onSnapshot } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { collection, addDoc, query, getDocs, onSnapshot, deleteDoc } from 'firebase/firestore';
 // import { setupPosts } from '../views/muro';
 import { auth, db } from './configfirebase'; //db
 // import { getFirestore } from "https://www.gstatic.com/firebasejs/9.17.1/firebase-firestore.js"
 
-// CREA UN USUARIO NUEVO CON EMAIL Y PASSWORD
+
+// **** CREA UN USUARIO NUEVO CON EMAIL Y PASSWORD ****
+
 export const createUserEmail = (email, password) => {
     createUserWithEmailAndPassword(auth, email, password)
         .then((userCredential) => {
@@ -21,21 +23,25 @@ export const createUserEmail = (email, password) => {
         });
 };
 
-// LOGUEO CON EMAIL Y PASSWORD
+// **** LOGUEO CON EMAIL Y PASSWORD ****
+
 export const signInUserEP = (email, password) => {
     return signInWithEmailAndPassword(auth, email, password);
 };
 
-// CREAR POST EN FIRESTORE DESDE EL TEMPLATE
-export const createPost = async (comment) => {
+// **** CREAR POST EN FIRESTORE DESDE EL TEMPLATE ****
+
+export const createPost = async ( user, comment) => {
     // Add a new document with a generated id.
     const documentReference = await addDoc(collection(db, "posts"), {
-        comment
+        user, date: new Date().getDate() + '/' + (new Date().getMonth() + 1) + '/' + new Date().getFullYear(), comment,
     });
     console.log("Document written with ID: ", documentReference.id);
 };
 
-// TRAE LOS POST DE FIRESTORE
+
+// **** TRAE LOS POST DE FIRESTORE ****
+
 export const getPosts = async () => {  // se asegura de que la función devuelva una promesa
     // Add a new document with a generated id.
     const documentsReference = await getDocs(collection(db, "posts")); // hace que JavaScript espere hasta que la promesa responda y devuelve su resultado.
@@ -44,55 +50,35 @@ export const getPosts = async () => {  // se asegura de que la función devuelva
     return documentsReference.docs;
 };
 
+// **** TRAE LOS POST DE FIRESTORE Y LOS MUESTRA EN TIEMPO REAL ****
+
 export const getPostsSnapShot = async () => {  // se asegura de que la función devuelva una promesa
     // Add a new document with a generated id.
     const queryDb = query(collection(db, "posts")); // hace que JavaScript espere hasta que la promesa responda y devuelve su resultado.
-    
-
+    console.log('queryDb')
     const unsubscribe = onSnapshot(queryDb, (querySnapshot) => {
-        console.log(querySnapshot);
+        console.log('querySnapshot', querySnapshot.docs); // Muestra las props de querysnapshot, una de ellas es "docs", del que muestra sus elementos como objeto
 
-        const posts = [];
+        const posts = [];   
         querySnapshot.forEach((post) => {
-            const itemPost = {
-             comentario: comment,
-             user: 'nelly'
-            }
+            //console.log('post', post) // Muestra los posts creados de Firestore(Objetos) con todas sus props
+            // console.log('post', post.data()) // El Método data(),Transforma el Objeto de Firestore a Objeto de JS
+            // const itemsPosts=post.data();
+            // console.log('items', itemsPosts);   //Muestra los posts con sus propiedades user y comment
 
-            posts.push(itemPost);
+            // itemsPosts = {
+            //     user, date, comment,
+            // }
 
-            posts.push(post.data().comment);
+            // posts.push(post.data());
+
+            // posts.push(post.data().comment);
             // console.log(post.data().comment);
         });
-    })
-    
+    });
+
     return queryDb.posts;
 };
-
-
-// //
-// export const getPosts = async ()  => {  // se asegura de que la función devuelva una promesa
-// 	// Add a new document with a generated id.
-// 		const documentsReference = query(collection(db, "posts")); // hace que JavaScript espere hasta que la promesa responda y devuelve su resultado.
-// 		const unsubscribe = onSnapshot(documentsReference, (querySnapshot) => {
-// 			const posts = [];
-// 			querySnapshot.forEach()
-// 		})
-// 		console.log("Documents ", documentsReference);
-// 	  	return documentsReference.docs;
-// };
-
-// AGREGAR POST CREADO DE FIRESTORE AL MURO EN TIEMPO REAL
-// const addNewPostInWall =
-// const q = query(collection(db, "cities"), where("state", "==", "CA"));
-// const unsubscribe = onSnapshot(q, (querySnapshot) => {
-// 	const cities = [];
-// 	querySnapshot.forEach((doc) => {
-// 		cities.push(doc.data().name);
-// 	});
-// 	console.log("Current cities in CA: ", cities.join(", "));
-// });
-
 
 // // Si estamos autenticados al inicio de sesion
 // onAuthStateChanged (auth, async (user) => { // Es asíncrono porque se está consultando una BD
@@ -104,3 +90,18 @@ export const getPostsSnapShot = async () => {  // se asegura de que la función 
 // 				alert('Usuario no está logueado');
 // 			}
 // });
+
+// **** lOGOUT USER **** OK
+export const logout = () => {
+    signOut(auth).then(() => {
+        // Sign-out successful.
+        // console.log('logout');
+        window.location.hash = '#/login';
+    })
+        .catch((error) => {
+            // An error happened.
+        });
+};
+
+export const deletePost = () => deleteDoc(post(db, "posts"));
+console.log('delete', deletePost);
--- src/views/muro.js
+++ src/views/muro.js
@@ -1,35 +1,45 @@
 // import { doc } from 'firebase/firestore';
-import { createPost, getPosts, getPostsSnapShot } from '../lib/firebase'
+import { createPost, getPosts, getPostsSnapShot, logout, deletePost } from '../lib/firebase'
 
 export default () => {
     const viewWall = `
-    <div class ="backgroundMuro"> 
+    <div class ="backgroundMuro">
         <header>
             <div class="headerWall"
                 <figure>
                     <img class="logoTravellxWall" src="/Imagenes/titleLogoTravellx.png" alt="logoTravellx">
                 </figure>
-                <input class="logoutWall" id ="logoutBtn" type="submit" value="Logout">
+                <button type="button" class="logoutWall" id ="logoutBtn">Log Out</button>
             </div>
         </header>
-        <div class="userWellcome">
-                    <label class="userName">HOLA,</label>
-                    <p class ="userEmailWellcome" type="text" name="userEmail"></p> 
-        </div>
-        <div class = "createPost">
-            <div class = "newPost" id= "newPost">
-            <textarea class="textPost" id="idtextPost" name="textarea" rows="5" cols="40">Comparte aquí tus experiencias...</textarea>
-                <button type="button" class="postBtn" id="idCreateBtn">PUBLICAR</button>
+
+        <section class="wallTemplate">
+            <div class="userWellcome">
+                        <label class="userName">HOLA,</label>
+                        <p class ="userEmailWellcome" type="text"></p> 
             </div>
-            <div class ="showPostCreated">
+            <div class="createPost" id="createPost">
+                <input type="hidden" id="post-id" value="">
+                
+                
+                <div class="newPost" id="newPost">
+                    <input type="text" class="userName" id="iduserName" placeholder="User Name">
+                    <textarea class="textPost" id="idtextPost" name="textarea" rows="5" cols="40" placeholder="Comparte aquí tus experiencias..."></textarea>
+                    <div class="btnpost">    
+                        <button type="button" class="postBtn" id="idCreateBtn">PUBLICAR</button>
+                    </div>
+                </div>
+                <div class ="showPostCreated">
+                </div>
             </div>
-        </div>
+        </section>
     </div>          
     `;
-
+    // <p id="idDatetime">fecha y hora</p> //
     const sectionElement = document.createElement('section');
     sectionElement.innerHTML = viewWall;
     const showPostCreated = sectionElement.querySelector('.showPostCreated');
+    // const createPostTemplate = sectionElement.querySelector('#createPost').innerHTML = "";
 
     // se llama al metodo getposts
     getPosts().then((posts) => {   //  obteniendo Todos los posts, array de objetos de la coleccion posts
@@ -42,71 +52,97 @@ export default () => {
     // FIN DEL LOAD
 
 
-    // CREAR POST
+    // **** lOGOUT USER ****
+    sectionElement.querySelector('#logoutBtn').addEventListener('click', (e) => {
+        e.preventDefault();
+        logout();
+    });
+
+
+    // **** CREAR POST ****
+
     const crearPost = sectionElement.querySelector('#idCreateBtn');
     // console.log (crearPost);
     crearPost.addEventListener("click", () => {
-
+        const userNamePost = sectionElement.querySelector('#iduserName').value;
+        // sectionElement.querySelector('#idDatetime').innerHTML = Date()
         const postMessage = sectionElement.querySelector('#idtextPost').value;
         // console.log(postMessage);
-        createPost(postMessage);
+        createPost(userNamePost, postMessage);
 
         // // se llama al metodo getpost (Encapsular en un metodo ---)
         getPosts().then((posts) => {   //  obteniendo Todos los posts, array de objetos de la coleccion posts
             // console.log(posts); // 
             const htmlPosts = publicatedPost(posts);  // se genera el html con los posts x cada post, publicando posts 
             showPostCreated.innerHTML = htmlPosts;   // se inyecta en el div clase showPostCreated
-    
-        });
-    
 
-});
-return sectionElement; // en el valor de retorno es donde se va a mostrar los elementos del Dom
+        });
+        // return createPostTemplate;
+
+    });
+    return sectionElement; // en el valor de retorno es donde se va a mostrar los elementos del Dom
+
+
 };
 
 
-// PUBLICAR POSTS EN EL MURO
+// **** PUBLICAR POSTS EN EL MURO ****
+
 export const publicatedPost = (posts) => {   // 
     if (posts.length) {      //Si existen datos, los va a recorrer
         let template = '<div class="templatePosts">';
 
-        posts.forEach(post => {
+        posts.forEach(post => {     // Todos los post de firestore con sus propiedades incluído document-data(objeto)-user y comment
             // console.log(post);
-            const dataPost = post.data();
+            const dataPost = post.data(); // Todos los objetos-js(posts) creados con sus propiedades
+            
             const divPost = `
                 <div class="templatePost">
                     <div>
-                        <h3>hora</h1>
-                        <h3>usuario</h1>
-                    </div>
-                    <div>
+                        <h3 class="userName" id="iduserName" type="text">${dataPost.user}</h3>
+                        <h3 class="userName" id="iduserName" type="text">${dataPost.date}</h3>
                         <p class="createdPost" id="idcreatedPost" type="text">${dataPost.comment}</p>
                     </div>
                     <div class="buttonsEdit">
                         <button type="button" class="editBtn" id ="ideditBtn">EDITAR</button>
-                        <button type="button" class="editBtn" id ="idCancelBtn">CANCELAR</button>
-                        <button type="button" class="editBtn" id ="idDeleteBtn">ELIMINAR</button>
+                        <button type="button" class="idCancelBtn" id ="idCancelBtn">CANCELAR</button>
+                        <button type="button" class="idDeleteBtn" id ="idDeleteBtn">ELIMINAR</button>
                     </div>
                 </div>    
             `;
-            template += divPost
+            template += divPost;
         });
 
         template += '</div>';
+        
         // template.innerHTML = template;
+        console.log(template);
+        // const deletePost = template.querySelectorAll('.showPostCreated');
+        // console.log('deletePost', deletePost);
+
+        // deletePost.forEach((boton) => {
+        //     boton.addEventListener('click', () => {
+        //         console.log('borrando');
+        //     })
+
+        // btn.addEventListener('click', ({ target: { dataset } }) => {
+        //     /* eslint-disable no-alert, no-restricted-globals */
+        //     const confirmDelete = confirm('¿Segura que deseas eliminar este post?');
+        //     /* eslint-disable no-alert, no-restricted-globals */
+        //     // si el usuario confirma...
+        //     if (confirmDelete) {
+        //         deletePost(dataset.id); // se elimina con la fx q se trae de f/firestore
+        //     }
+        // });
+        // });
         return template;
 
     } else {
         // template.innerHTML = '<li> No hay posts aún </li>';
         return ' No hay posts aún ';
-    }
+    };
 };
 
-// export const Logout =() => {
-//     const logoutBtn = document.getElementById ('logoutBtn')
-//     logoutBtn.addEventListener('click', () => {
-//         // Firebase.auth.signOut();
-//         location.href = '#/login';
-//     });
-// }
+
+//* <h4 class="userName" id="idDatetime" type="text">${datePost}</h4>   */
 
